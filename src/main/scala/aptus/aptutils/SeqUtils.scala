0885a15d40a421659f7e5c4e0f385ee08385f106
Anthony Cros <cros.anthony@gmail.com>
2022-03-16 14:08:50 -0400

    Added more Iterable-like orderings

--- src/main/scala/aptus/aptutils/SeqUtils.scala
+++ src/main/scala/aptus/aptutils/SeqUtils.scala
@@ -24,27 +24,11 @@ object SeqUtils {
   }
   
   // ===========================================================================
-  private[aptus] def seqOrdering[T : Ordering]: Ordering[Seq[T]] = new Ordering[Seq[T]] { // 210827123947
-      override def compare(x: Seq[T], y: Seq[T]): Int = {            
+  // note Ordering is invariant in T
+  private[aptus] def iterableOrdering[T : Ordering]: Ordering[Iterable[T]] = new Ordering[Iterable[T]] { // 210827123947
+      override def compare(x: Iterable[T], y: Iterable[T]): Int = {            
         val sizeComparison = x.size.compare(y.size)
-        
-        if (sizeComparison != 0) sizeComparison
-        else {
-          val ordering = implicitly[Ordering[T]]
   
-          x .zip(y)
-            .map { case (l, r) => ordering.compare(l, r) }
-            .find(_ != 0)
-            .getOrElse(0)
-        }
-      }
-    }
-    
-    // ---------------------------------------------------------------------------
-    private[aptus] def listOrdering[T : Ordering]: Ordering[List[T]] = new Ordering[List[T]] { // 210827123947
-      override def compare(x: List[T], y: List[T]): Int = {            
-        val sizeComparison = x.size.compare(y.size)
-        
         if (sizeComparison != 0) sizeComparison
         else {
           val ordering = implicitly[Ordering[T]]
@@ -57,6 +41,22 @@ object SeqUtils {
       }
     }
 
+    // ---------------------------------------------------------------------------
+    private[aptus] def seqOrdering[T : Ordering]: Ordering[Seq[T]] =
+      new Ordering[Seq[T]] {
+        override def compare(x: Seq[T], y: Seq[T]): Int = 
+          iterableOrdering.compare(x, y) }
+    
+    // ---------------------------------------------------------------------------
+    private[aptus] def listOrdering[T : Ordering]: Ordering[List[T]] = new Ordering[List[T]] {
+      override def compare(x: List[T], y: List[T]): Int = 
+          iterableOrdering.compare(x, y) }
+
+    // ---------------------------------------------------------------------------
+    private[aptus] def arrayOrdering[T : Ordering]: Ordering[Array[T]] = new Ordering[Array[T]] {
+      override def compare(x: Array[T], y: Array[T]): Int =  
+          iterableOrdering.compare(x, y) }
+         
 }
 
 // ===========================================================================

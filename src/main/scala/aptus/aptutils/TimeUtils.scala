7c4a7a45a261b41b8f7f295c74d292248906a945
Anthony Cros <cros.anthony@gmail.com>
2022-03-16 15:26:26 -0400

    Added TimeUtils

--- src/main/scala/aptus/aptutils/TimeUtils.scala
+++ src/main/scala/aptus/aptutils/TimeUtils.scala
@@ -1,9 +1,28 @@
 package aptus
 package aptutils
 
+import java.time._
+import java.time.format.DateTimeFormatter
+
 // ===========================================================================
 object TimeUtils {
 
+  // ===========================================================================
+  def currentZoneOffset() = OffsetDateTime.now().getOffset() // eg for Toronto, will be -4 or -5 hours (based on daylight saving time) - don't cache
+
+  // ---------------------------------------------------------------------------
+  lazy val DefaultZoneId = ZoneId.systemDefault() // eg "America/Toronto" (-5 hours)
+
+  // ---------------------------------------------------------------------------
+  lazy val IsoFormatterInstant        = DateTimeFormatter.ISO_INSTANT
+  lazy val IsoFormatterLocalDateTime  = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+  lazy val IsoFormatterOffsetDateTime = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+  lazy val IsoFormatterZonedDateTime  = DateTimeFormatter.ISO_ZONED_DATE_TIME
+
+  lazy val IsoFormatterLocalDate      = DateTimeFormatter.ISO_LOCAL_DATE
+  lazy val IsoFormatterLocalTime      = DateTimeFormatter.ISO_LOCAL_TIME
+
+  // ===========================================================================
   def elapsed[A](block: => A): (A, Long) = {
     val start = System.currentTimeMillis()
     val result = block

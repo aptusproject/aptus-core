35f944cb1dc3eec9356be33d98a0a0c8d7d9cf36
Anthony Cros <cros.anthony@gmail.com>
2022-03-17 13:40:50 -0400

    Added CloseabledIterator (2)

---
+++ src/test/scala/aptustesting/CloseabledTests.scala
@@ -0,0 +1,29 @@
+package aptustesting
+
+import scala.util.chaining._
+import aptus._
+import utest._
+
+// ===========================================================================
+object CloseabledTests extends TestSuite {
+  
+  val tests = Tests {       
+    test(compare(
+        // or eg "/tmp/myfile".streamFileLines3().pipe(transform)
+        CloseabledIterator.fromValues("hello", "world").pipe(transform), 
+        Seq("HE", "LLO", "WO", "RLD")))
+  }
+
+  // ---------------------------------------------------------------------------
+  private def transform(citr: CloseabledIterator[String]) =
+    citr
+      .map(_.toUpperCase)
+      .flatMap { s => 
+        Seq(
+          s.substring(0, 2),
+          s.substring(2)) }
+      .consumeToList
+
+}
+
+// ===========================================================================

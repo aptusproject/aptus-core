6ed61bbe16a293e5be2e2b069d47629d7cbc146c
Anthony Cros <cros.anthony@gmail.com>
2022-09-26 11:48:58 -0400

    Improvements to GsonParser

--- src/main/scala/aptus/aptjson/GsonParser.scala
+++ src/main/scala/aptus/aptjson/GsonParser.scala
@@ -1,6 +1,7 @@
 package aptus
 package aptjson
 
+import scala.util.chaining._
 import scala.collection.JavaConverters._
 import com.google.gson._
 
@@ -18,23 +19,39 @@ object GsonParser {
         if (first == '{') Left (stringToJsonObject(value))
         else              Right(stringToJsonArray (value)) }
 
-  // ---------------------------------------------------------------------------
-  def stringToJsonObject(value: JsonObjectString): JsonObject =
-    JsonParser
-      .parseString(value)
-      .getAsJsonObject
+  // ===========================================================================
+  @inline def stringToJsonElement(value: JsonObjectString): JsonElement = JsonParser.parseString(value)
 
-  // ---------------------------------------------------------------------------
-  def stringToJsonArray(value: JsonArrayString): JsonArray =
-    JsonParser
-      .parseString(value)
-      .getAsJsonArray
+    // ---------------------------------------------------------------------------
+    def stringToJsonObject   (value: JsonObjectString): JsonObject    = stringToJsonElement(value).getAsJsonObject
+    def stringToJsonPrimitive(value: JsonObjectString): JsonPrimitive = stringToJsonElement(value).getAsJsonPrimitive
+    def stringToJsonArray    (value: JsonArrayString) : JsonArray     = stringToJsonElement(value).getAsJsonArray
 
   // ---------------------------------------------------------------------------
   def stringToJsonObjects(value: JsonArrayString): Iterator[JsonObject] =
     stringToJsonArray(value)
       .iterator().asScala
       .map(_.getAsJsonObject)
+
+  // ---------------------------------------------------------------------------
+  def stringToPrimitiveValueAny (value: String):     Any  = jsonPrimitiveToAny(stringToJsonPrimitive(value))
+  def stringToPrimitiveValueAnys(value: String): Seq[Any] = stringToJsonArray(value).asScala.toList.map(_.getAsJsonPrimitive.pipe(jsonPrimitiveToAny))
+
+  // ===========================================================================
+  /** unfortunately no access to underlying Object value in gson; TODO: t201103154749 - look into cost of reflection setAccessible(true) or do a PR */
+  def jsonPrimitiveToAny(x: JsonPrimitive): Any =
+         if (x.isString ) x.getAsString
+    else if (x.isBoolean) x.getAsBoolean
+    else
+      /*
+       * TODO: can it actually be any of:
+       * - AtomicInteger, AtomicLong,
+       * - DoubleAccumulator, DoubleAdder,
+       * - LongAccumulator, LongAdder */
+      x.getAsNumber match { // TODO: in which case will "value: Object" actually already be a Number (aot a LazilyParsedNumber)?
+        case x: java.math.BigDecimal => BigDecimal(x)
+        case x: java.math.BigInteger => BigInt    (x)
+        case x                       => x.doubleValue() /* may also be com.google.gson.internal.LazilyParsedNumber, which stores it as a String (but only accessible as int, long, float and double, ...) */ }
 
 }
 

e0435c240dd9ff4806f87cee991798e48687bd1d
Anthony Cros <cros.anthony@gmail.com>
2023-06-23 15:18:35 -0400

    improved _AptusPath

--- src/main/scala/aptus/aptmisc/AptusPath.scala
+++ src/main/scala/aptus/aptmisc/AptusPath.scala
@@ -15,7 +15,8 @@ trait _AptusPath {
   override def toString: String = path
 
   // ---------------------------------------------------------------------------
-  def exists(): Boolean = NioFiles.exists(nioPath)
+  def        exists(): Boolean =  NioFiles.exists(nioPath)
+  def doesNotExists(): Boolean = !NioFiles.exists(nioPath)
 
   protected def _isFile   (): Boolean = NioFiles.isRegularFile (nioPath)
   protected def _isDir    (): Boolean = NioFiles.isDirectory   (nioPath)
@@ -26,13 +27,17 @@ trait _AptusPath {
   def writeFile(content: String): Unit = content.writeFileContent(path) /* see String_ */
 
   def ensureDir   (): DirPath = if (_isDir()) path else createNewDir()
-  def createNewDir(): DirPath = { ioFile.mkdir(); path }
+  def createNewDir(): DirPath = { assert(ioFile.mkdirs(), path); path }
+
+  def ensureEmpty(): DirPath = { listNames().assert(_.isEmpty); path }
 
   // ===========================================================================
   // note: may be removed it immediately ("On some operating systems it may not be possible to remove a file when it is open and in use by this Java virtual machine or other programs")
-  def removeFile(): Unit = { require(_isFile(), path); NioFiles.delete(nioPath); () }
+  def removeFile (): Unit     = { require(_isFile(), path); NioFiles.delete(nioPath); () }
+  def removeFile2(): FilePath = { require(_isFile(), path); NioFiles.delete(nioPath); path }
 
-  def removeFileIfExists(): Unit = { if (_isFile()) { NioFiles.delete(nioPath) }; () }
+  def removeFileIfExists (): Unit = { if (_isFile()) { NioFiles.delete(nioPath) }; () }
+  def removeFileIfExists2(): FilePath = { if (_isFile()) { NioFiles.delete(nioPath) }; path }
 
   def moveTo(path: OutputFilePath) = NioFiles.move(nioPath, NioPaths.get(path), Array[CopyOption]():_*)
 
@@ -48,14 +53,15 @@ trait _AptusPath {
   // ---------------------------------------------------------------------------
   @deprecated def emptyDirNonRecursively() = { require(listDirNames().isEmpty); removeFiles() } // must not  contains sub-directories
 
-  def removeFiles() = {
+  def removeFiles(): String = {
     require(_isDir(), path)
 
     listFileNames()
       .map { n => s"${path}/${n}".path }
       .filter (_.isFile    ())
       .foreach(_.removeFile())
-  }
+
+    path }
 
   // ===========================================================================
   def listContent(): List[Path] = Option(ioFile.list()).map(_.toList).getOrElse(illegalArgument("220926133236 - not a dir"))
@@ -73,6 +79,10 @@ trait _AptusPath {
     def listFilePathsRecursively(): List[FilePath] = listPathsRecursively().filter(new File(_).isFile())
     def listDirPathsRecursively (): List[DirPath]  = listPathsRecursively().filter(new File(_).isDirectory())
 
+    def listFilePathsRecursively(suffix: String): List[FilePath] = listFilePathsRecursively().filter(_.endsWith(suffix)) // convenient for file extensions
+
+  def name = ioFile.getName()
+
   // ===========================================================================
   //TODO: mkdirs, glob, ... (more needs to be ported from original aptus, see t210116165619)
 }
@@ -81,13 +91,19 @@ trait _AptusPath {
 private[aptus] final class AptusDirPath(override val path: String) extends _AptusPath {
   def  isEmpty(): Boolean = listContent().isEmpty
   def nonEmpty(): Boolean = !isEmpty()
+
+  def mkdir (): String = { s"mkdir    ${path}".systemCall(); path }
+  def mkdirs(): String = { s"mkdir -p ${path}".systemCall(); path } }
+
+// ===========================================================================
+// make private again
+/*private[aptus]*/ final class AptusFilePath(override val path: String) extends _AptusPath {
+  //def renameTo
 }
 
 // ===========================================================================
-private[aptus] final class AptusFilePath(override val path: String) extends _AptusPath
-
-// ===========================================================================
-private[aptus] final class AptusPath(override val path: String) extends _AptusPath { // TODO: as AnyVal?
+// make private again
+/*private[aptus]*/ final class AptusPath(override val path: String) extends _AptusPath { // TODO: as AnyVal?
 
     def dir  = new aptmisc.AptusDirPath (path)
     def file = new aptmisc.AptusFilePath(path)

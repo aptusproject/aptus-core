9e3e93416dfbcedbe8f8e776127b4026e4fdb806
Anthony Cros <cros.anthony@gmail.com>
2023-06-23 15:18:11 -0400

    added FileUtils' append utilities

--- src/main/scala/aptus/aptutils/FileUtils.scala
+++ src/main/scala/aptus/aptutils/FileUtils.scala
@@ -10,9 +10,19 @@ object FileUtils {
   def readFileContent(path: FilePath): Content      = { val src = io.Source.fromInputStream(inputStream(quickNormalize(path))); val content = src.mkString       ; src.close; content }
   def readFileLines  (path: FilePath): List[String] = { val src = io.Source.fromInputStream(inputStream(quickNormalize(path))); val lines   = src.getLines.toList; src.close; lines   }
 
-  def streamFileLines  (path: FilePath): (Iterator[String], Closeable) = { val src = io.Source.fromInputStream(inputStream(quickNormalize(path))); src.getLines -> src }
-  
-    // ---------------------------------------------------------------------------
+  def streamFileLines  (path: FilePath): (Iterator[String], Closeable) = {
+    val src = io.Source.fromInputStream(inputStream(quickNormalize(path)))
+
+sys.addShutdownHook { // TODO: only if in auto mode
+  println(s"shutting down: $path")
+  //src.close()
+  src.close()
+}
+
+  src.getLines -> src
+  }
+
+    // ===========================================================================
     private def inputStream(path: FilePath): InputStream =
       InputStreamUtils.smartInputStream(
         new BufferedInputStream(
@@ -20,13 +30,13 @@ object FileUtils {
 
   // ===========================================================================
   def writeContent     (path: FilePath, content: Content): FilePath = if (path.endsWith(".gz")) writeGzipContent(path, content) else writePlainContent(path, content)
-  def writePlainContent(path: FilePath, content: Content): FilePath = { val fw = plainFileWriter(path); fw.write(content); fw.close(); path }
-  def writeGzipContent (path: FilePath, content: Content): FilePath = { val fw =  gzipFileWriter(path); fw.write(content); fw.close(); path }
+  def writePlainContent(path: FilePath, content: Content): FilePath = { val fw = plainFileWriter(path, append = false); fw.write(content); fw.close(); path }
+  def writeGzipContent (path: FilePath, content: Content): FilePath = { val fw =  gzipFileWriter(path, append = false); fw.write(content); fw.close(); path }
 
   // ---------------------------------------------------------------------------
   def writeLines     (path: FilePath, values: Seq[Line]): FilePath = if (path.endsWith(".gz")) writeGzipLines(path, values) else writePlainLines(path, values)
-  def writePlainLines(path: FilePath, values: Seq[Line]): FilePath = { val fw = plainFileWriter(path); values.map(x => s"${x}\n").foreach(fw.write) /* TODO: flush often? */; fw.close(); path }
-  def writeGzipLines (path: FilePath, values: Seq[Line]): FilePath = { val fw =  gzipFileWriter(path); values.map(x => s"${x}\n").foreach(fw.write) /* TODO: flush often? */; fw.close(); path }
+  def writePlainLines(path: FilePath, values: Seq[Line]): FilePath = { val fw = plainFileWriter(path, append = false); values.map(x => s"${x}\n").foreach(fw.write) /* TODO: flush often? */; fw.close(); path }
+  def writeGzipLines (path: FilePath, values: Seq[Line]): FilePath = { val fw =  gzipFileWriter(path, append = false); values.map(x => s"${x}\n").foreach(fw.write) /* TODO: flush often? */; fw.close(); path }
 
   // ---------------------------------------------------------------------------
   def writeGzipLines(
@@ -35,7 +45,7 @@ object FileUtils {
         logModulo   : Option[Long => Unit])
         (iter: Iterator[Line])
       : OutputFilePath = {
-    val gfw = gzipFileWriter(out)
+    val gfw = gzipFileWriter(out, append = false)
 
     var counter: Long = 0
 
@@ -59,26 +69,55 @@ object FileUtils {
   }
 
   // ===========================================================================
+  def appendToPlainFile(out: FilePath, value: String): OutputFilePath = {
+    val fw = plainFileWriter(out, append = true)
+
+      fw.append(value)
+
+      fw.flush()
+      fw.close()
+
+    out } // TODO: return content rather?
+
+  // ---------------------------------------------------------------------------
+  def appendToGzipFile(out: FilePath, value: String): OutputFilePath = {
+    val fw = gzipFileWriter(out, append = true)
+
+      fw.append(value)
+
+      fw.flush()
+      fw.close()
+
+    out } // TODO: return content rather?
+
+  // ===========================================================================
   // writers
 
-  private def gzipFileWriter(path: FilePath): Writer = new OutputStreamWriter(syncFlushGzipOutputStream(path))
+  private def plainFileWriter(path: FilePath, append: Boolean): Writer =
+    new FileWriter(quickNormalize(path), append)
 
-  private def plainFileWriter(path: FilePath, append: Boolean = false): Writer = new FileWriter(quickNormalize(path), append)
+  // ---------------------------------------------------------------------------
+  private def gzipFileWriter(path: FilePath, append: Boolean): Writer =
+    new OutputStreamWriter(syncFlushGzipOutputStream(path, append))
 
   // ===========================================================================
   // streams
 
-  private def fileOutputStream(path: FilePath, append: Boolean = false): OutputStream = new FileOutputStream(quickNormalize(path), append)
+  private def fileOutputStream(path: FilePath, append: Boolean): OutputStream =
+    new FileOutputStream(quickNormalize(path), append)
 
+  // ---------------------------------------------------------------------------
   //https://stackoverflow.com/questions/3640080/force-flush-on-a-gzipoutputstream-in-java
-  private def syncFlushGzipOutputStream(path: FilePath): OutputStream =
-      new GZIPOutputStream(fileOutputStream(quickNormalize(path)), /*syncFlush = */true)
+  private def syncFlushGzipOutputStream(path: FilePath, append: Boolean): OutputStream =
+      new GZIPOutputStream(fileOutputStream(
+          path   = quickNormalize(path),
+          append = append),
+        /*syncFlush = */true)
 
   // ===========================================================================
   private def quickNormalize(path: FilePath): FilePath =
     if (!path.startsWith("~/")) path
     else                        s"${().fs.homeDirectoryPath()}/${path.drop(2)}" // eg ~/foo to /home/tony/foo
-
 }
 
 // ===========================================================================

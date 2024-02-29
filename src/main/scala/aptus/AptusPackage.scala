13c2d95ef229242d3a8010de8812496da5ebb55b
Anthony Cros <cros.anthony@gmail.com>
2024-02-29 14:23:28 -0500

    externalized data-related Seq operations (3)

--- src/main/scala/aptus/AptusPackage.scala
+++ src/main/scala/aptus/AptusPackage.scala
@@ -477,45 +477,19 @@ package object aptus
     def toListMap   [K, V](implicit ev: A <:< (K, V))                   = MapUtils.toListMap(coll)
     def toTreeMap   [K, V](implicit ev: A <:< (K, V), ord: Ordering[K]) = MapUtils.toTreeMap(coll)
 
-    // ---------------------------------------------------------------------------
-    def groupByWithListMap[K, V](f: A => K)(implicit ev: A =:= V): ListMap[K, Seq[V]] = groupByWithListMap(f, v => v)
-    def groupByWithListMap[K, V](f: A => K, g: A => V)           : ListMap[K, Seq[V]] = aptutils.MapUtils.groupByWithListMap(f, g)(coll.iterator)
-
-    // ---------------------------------------------------------------------------
-    def groupByKey           [K, V](implicit ev: A <:< (K, V)                  ):               Map[K, Seq[V]] = MapUtils.groupByKey           (coll.iterator.asInstanceOf[Iterator[(K, V)]])
-    def groupByKeyWithListMap[K, V](implicit ev: A <:< (K, V)                  ):           ListMap[K, Seq[V]] = MapUtils.groupByKeyWithListMap(coll.iterator.asInstanceOf[Iterator[(K, V)]])
-    def groupByKeyWithTreeMap[K, V](implicit ev: A <:< (K, V), ord: Ordering[K]): immutable.TreeMap[K, Seq[V]] = MapUtils.groupByKeyWithTreeMap(coll.iterator.asInstanceOf[Iterator[(K, V)]])
-
-    // ---------------------------------------------------------------------------
-    def groupByAdjacency[B](f: A => B): Seq[(B, Seq[A])] = MapUtils.groupByAdjacency(coll)(f)
-
     // ===========================================================================
-    def countBySelf: List[(A, Int)] = coll.groupBy(identity).view.map { x => x._1 -> x._2.size }.toList.sortBy(-_._2) // TODO: t211004120452 - more efficient version
+    def data = new aptmisc.Data[A](coll)
 
-    /** maintains order (if presorted for instance) */
-    def countBySelfWithOrder: List[(A, Int)] = coll.map(x => x -> x).groupByKeyWithListMap.toList.map { x => x._1 -> x._2.size }.sortBy(-_._2) // TODO: t211004120452 - more efficient version
+      // ---------------------------------------------------------------------------
+      // shorthands for most commonly used
+      def groupByKey           [K, V](implicit ev: A <:< (K, V)):     Map[K, Seq[V]] = data.groupByKey
+      def groupByKeyWithListMap[K, V](implicit ev: A <:< (K, V)): ListMap[K, Seq[V]] = data.groupByKeyWithListMap
 
-    // TODO: t220929165238 - more efficient version (see groupByKey)
-    def countByKey[K, V](implicit ev: A <:< (K, V)): Seq[(Count, K)] = groupByKey.map { case (k, v) => v.size -> k }.toList.sortBy(-_._1)
+      def countBySelf: List[(A, Int)] = data.countBySelf
 
-    // ===========================================================================
-    import aptutils.joining.FluencyDomain._
+      def innerJoin[B](that: Seq[B]): joining.FluencyDomain.InnerJoin[A, B] = data.innerJoin(that)
 
-    def innerJoin[B](that: Seq[B]): InnerJoin[A, B] = new InnerJoin[A, B](coll, that)
-    def  leftJoin[B](that: Seq[B]):  LeftJoin[A, B] = new  LeftJoin[A, B](coll, that)
-    def rightJoin[B](that: Seq[B]): RightJoin[A, B] = new RightJoin[A, B](coll, that)
-    def outerJoin[B](that: Seq[B]): OuterJoin[A, B] = new OuterJoin[A, B](coll, that)
-
-    // ---------------------------------------------------------------------------
-    def innerCoGroup[B](that: Seq[B]): InnerCoGroup[A, B] = new InnerCoGroup[A, B](coll, that)
-    def  leftCoGroup[B](that: Seq[B]):  LeftCoGroup[A, B] = new  LeftCoGroup[A, B](coll, that)
-    def rightCoGroup[B](that: Seq[B]): RightCoGroup[A, B] = new RightCoGroup[A, B](coll, that)
-    def outerCoGroup[B](that: Seq[B]): OuterCoGroup[A, B] = new OuterCoGroup[A, B](coll, that)
-
-    // ===========================================================================
-    /** no prior grouping of key/values */ def pivot          [K, V](implicit ev: A <:< (K,     V )): ListMap[V, Seq[K]] = aptutils.PivotingUtils.pivot          (coll.asInstanceOf[Seq[(K, V)]])
-    /** no prior grouping of key/values */ def pivotAndFlatten[K, V](implicit ev: A <:< (K,     V )):    Seq[(V,     K)] = aptutils.PivotingUtils.pivotAndFlatten(coll.asInstanceOf[Seq[(K, V)]])
-    /** pre-grouped by keys */             def pivotPreGrouped[K, V](implicit ev: A <:< (K, Seq[V])): ListMap[V, Seq[K]] = aptutils.PivotingUtils.pivotPreGrouped(coll.asInstanceOf[Seq[(K, Seq[V])]])
+      /** no prior grouping of key/values */ def pivot[K, V](implicit ev: A <:< (K, V)): ListMap[V, Seq[K]] = data.pivot
 
     // ===========================================================================
     def toOptionalSeq[B](implicit ev: A <:< Option[B]): Option[Seq[B]] = if (coll.contains(None)) None else Some(coll.map(_.get))
@@ -832,8 +806,6 @@ package object aptus
 
     // ---------------------------------------------------------------------------
     implicit class JavaMap_[K, V](map: java.util.Map[K, V]) {
-      def javaToScala: Map[K, V] = CollectionConverters.asScala(map).toMap }
-
-}
+      def javaToScala: Map[K, V] = CollectionConverters.asScala(map).toMap } }
 
 // ===========================================================================

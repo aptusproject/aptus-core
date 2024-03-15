5c74ffb0c53a6873efb55ff86487e34840ca340d
Anthony Cros <cros.anthony@gmail.com>
2024-03-15 15:39:52 -0400

    added DoubleStats header names

--- src/main/scala/aptus/aptmisc/DoubleStats.scala
+++ src/main/scala/aptus/aptmisc/DoubleStats.scala
@@ -5,22 +5,33 @@ package aptmisc
 // TODO: add a fuller version with size, kurtosis, skewness, geometric/quadratic means, ...
 /** for low-tech stats (e.g. data exploration) */
 case class DoubleStats(
-    min   : Double,
-    mean  : Double, // arthmetic's
-    stdev : Double, // population's
-    median: Double,
-    IQR   : Double,
-    max   : Double) {
+      min   : Double,
+      mean  : Double, // arthmetic's
+      stdev : Double, // population's
+      median: Double,
+      IQR   : Double,
+      max   : Double) {
 
-  override def toString: String = formatDefault
-    def formatDefault: String =
+    def coefficientOfVariation: Double = stdev / mean
+
+    override def toString: String = formatDefault
+      def formatDefault: String =
+        Seq(
+            "min"    -> min,
+            "mean"   -> mean,
+            "stdev"  -> stdev,
+            "median" -> median,
+            "IQR"    -> IQR,
+            "max"    -> max)
+          .mkString(", ") }
+
+  // ===========================================================================
+  object DoubleStats {
+    def formatHeaderFields: Seq[String] = // eg for convenient {C,T}SV creation
       Seq(
-          "min"    -> min,
-          "mean"   -> mean,
-          "stdev"  -> stdev,
-          "median" -> median,
-          "IQR"    -> IQR,
-          "max"    -> max)
-        .mkString(", ") }
+        "min",
+          "mean"  , "stdev",
+          "median", "IQR",
+        "max") }
 
 // ===========================================================================

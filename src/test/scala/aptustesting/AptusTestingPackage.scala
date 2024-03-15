894e4f9ecde356a2274294b9ecb02a9f36bb36d0
Anthony Cros <cros.anthony@gmail.com>
2024-03-15 15:30:21 -0400

    test utilities improvements

--- src/test/scala/aptustesting/AptusTestingPackage.scala
+++ src/test/scala/aptustesting/AptusTestingPackage.scala
@@ -9,14 +9,30 @@ package object aptustesting {
   
   // ===========================================================================
   def noop   [A](actual: A)             : Unit = compare(actual, actual)
-  def compare[A](actual: A, expected: A): Unit = { Predef.assert(actual == expected, message = s"\n\t${expected}\n\t${actual}") }
+  def compare[A](actual: A, expected: A): Unit = { Predef.assert(actual == expected, message = s"\n\texpected: |${expected}|\n\tactual:   |${actual}|") }
   
   def compare      [A](enabled: Boolean)(actual: => A, expected: A): Unit = { if (enabled) compare(actual, expected) else () }
   def compareIfUnix[A]                  (actual: => A, expected: A): Unit = compare(aptus.system.isUnix())(actual, expected)
 
   // ---------------------------------------------------------------------------
-  def fail[$Throwable <: Throwable : ClassTag](f: => Any)              = { _fail[$Throwable](f, msgOpt = None) }
-  def fail[$Throwable <: Throwable : ClassTag](f: => Any, msg: String) = { _fail[$Throwable](f, msgOpt = Some(msg)) }
+  def isTrue(actual: Boolean): Unit = compare[Boolean](actual, true)
+  def isFlse(actual: Boolean): Unit = compare[Boolean](actual, false)
+
+  // ---------------------------------------------------------------------------
+  def isSome[T](actual: Option[T], someValue: T): Unit = compare[Option[T]](actual, Some(someValue))
+  def isNone[T](actual: Option[T])              : Unit = compare[Option[T]](actual, Option.empty[T])
+
+  // ---------------------------------------------------------------------------
+  def fail[$Throwable <: Throwable : ClassTag](f: => Any)             : Unit = { _fail[$Throwable](f, msgOpt = None) }
+  def fail[$Throwable <: Throwable : ClassTag](f: => Any, msg: String): Unit = { _fail[$Throwable](f, msgOpt = Some(msg)) }
+
+    // ---------------------------------------------------------------------------
+    def failAssertion(f: => Any)             : Unit = fail[java.lang.AssertionError]          (f)
+    def failAssertion(f: => Any, msg: String): Unit = fail[java.lang.AssertionError]          (f, msg)
+    def failArgument (f: => Any)             : Unit = fail[java.lang.IllegalArgumentException](f)
+    def failArgument (f: => Any, msg: String): Unit = fail[java.lang.IllegalArgumentException](f, msg)
+    def failState    (f: => Any)             : Unit = fail[java.lang.IllegalStateException]   (f)
+    def failState    (f: => Any, msg: String): Unit = fail[java.lang.IllegalStateException]   (f, msg)
 
   // ===========================================================================
   private def _fail[$Throwable <: Throwable : ClassTag](f: => Any, msgOpt: Option[String]) = {

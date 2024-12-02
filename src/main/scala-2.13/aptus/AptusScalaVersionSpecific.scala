fa34c9071baa1f67c032e9da55cdb0d4233edd04
Anthony Cros <cros.anthony@gmail.com>
2024-12-01 19:39:34 -0500

    adapted scala-version specific files to newer aliases

--- src/main/scala-2.13/aptus/AptusScalaVersionSpecific.scala
+++ src/main/scala-2.13/aptus/AptusScalaVersionSpecific.scala
@@ -1,12 +1,11 @@
 package aptus
 
 // ===========================================================================
-trait AptusScalaVersionSpecific {
+trait AptusScalaVersionSpecific { // 2.13
   type Items[$Items <: Items[_, _], $Item] = aptmisc.Items[$Items, $Item]
 
   // ---------------------------------------------------------------------------
-  import collection.immutable.ListMap
-  def listMap[K, V](values: Seq[(K, V)])          : ListMap[K, V] = ListMap.from(values)
+  def listMap[K, V](values: Seq[(K, V)])          : ListMap[K, V] = collection.immutable.ListMap.from(values)
   def listMap[K, V](value1: (K, V), more: (K, V)*): ListMap[K, V] = listMap[K, V](value1 +: more) }
 
 // ===========================================================================
--- src/main/scala-3/aptus/AptusScalaVersionSpecific.scala
+++ src/main/scala-3/aptus/AptusScalaVersionSpecific.scala
@@ -1,12 +1,11 @@
 package aptus
 
 // ===========================================================================
-trait AptusScalaVersionSpecific {
+trait AptusScalaVersionSpecific { // 3.x
   // FIXME: can't get this to work with scala 3: type Items[$Items <: Items[_, _], $Item] = aptmisc.Items[$Items, $Item]
 
   // ---------------------------------------------------------------------------
-  import collection.immutable.ListMap
-  def listMap[K, V](values: Seq[(K, V)])          : ListMap[K, V] = ListMap.from(values)
+  def listMap[K, V](values: Seq[(K, V)])          : ListMap[K, V] = collection.immutable.ListMap.from(values)
   def listMap[K, V](value1: (K, V), more: (K, V)*): ListMap[K, V] = listMap[K, V](value1 +: more) }
 
 // ===========================================================================

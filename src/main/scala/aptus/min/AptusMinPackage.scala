441bcffe394f53d20db0334b3cd9bc53c342f460
Anthony Cros <cros.anthony@gmail.com>
2024-12-01 19:40:31 -0500

    added 'minimal' aptus package

---
+++ src/main/scala/aptus/min/AptusMinPackage.scala
@@ -0,0 +1,7 @@
+package aptus
+
+// ===========================================================================
+/** minimal aptus, to import as: import aptus.min._ */
+package object min extends AptusMinimal
+
+// ===========================================================================
---
+++ src/main/scala/aptus/min/AptusMinimal.scala
@@ -0,0 +1,15 @@
+package aptus
+package min
+
+import apttraits._
+
+// ===========================================================================
+/** 230927172554 - have your prototyping package object extend this trait to save imports */
+trait AptusMinimal
+    extends AptusBooleanShorthands
+       with AptusDummyImplicitShorthand
+       with AptusNullShorthands
+       with AptusChaining
+       with AptusMinExtensions
+
+// ===========================================================================

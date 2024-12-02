d0aed15aaf617257ccc72bc721bb2dc2c518507e
Anthony Cros <cros.anthony@gmail.com>
2024-12-01 19:36:25 -0500

    added @nonovrd and @abstrct annotations

--- src/main/scala/aptus/AptusAnnotations.scala
+++ src/main/scala/aptus/AptusAnnotations.scala
@@ -1,6 +1,7 @@
 package aptus
 
 // ===========================================================================
+/* NOTE: none of those are enforced, they just act as documentation */
 trait AptusAnnotations { import scala.annotation.StaticAnnotation
 
   /** to convey that something isn't meant to remain in production code */
@@ -14,12 +15,15 @@ trait AptusAnnotations { import scala.annotation.StaticAnnotation
 
   /** to convey explicitly that something is intended to be overriden in some situations (as opposed to having forgotten to make it final). */
   class nonfinl     (val message: String = "") extends StaticAnnotation
+  class nonovrd     (val message: String = "") extends StaticAnnotation
   class notsealed   (val message: String = "") extends StaticAnnotation
   class notprivate  (val message: String = "") extends StaticAnnotation
   class notprotected(val message: String = "") extends StaticAnnotation
 
+  /** just to highlight it*/
+  class abstrct (val message: String = "") extends StaticAnnotation
+
   /** to convey that the order of the following code is important */
-  class ordermatters(val message: String = "") extends StaticAnnotation
-}
+  class ordermatters(val message: String = "") extends StaticAnnotation }
 
 // ===========================================================================

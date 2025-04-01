d1a611193785f56dadcf81d702dee568e29998fe
Anthony Cros <cros.anthony@gmail.com>
2025-04-01 12:05:09 -0400

    remove Items alias from specific scala file (moved to common)

--- src/main/scala-2.13/aptus/AptusScalaVersionSpecific.scala
+++ src/main/scala-2.13/aptus/AptusScalaVersionSpecific.scala
@@ -4,9 +4,6 @@ package aptus
 trait AptusScalaVersionSpecific { // 2.13
   trait WTT[T] // TODO: where did this go? removed after 2.13.13? scala.reflect.runtime.universe.WeakTypeTag[T] - object runtime is not a member of package reflect
     object WTT { implicit def to[T]: WTT[T] = new WTT[T]{} }
-
-  // ---------------------------------------------------------------------------
-  type Items[$Items <: Items[_, _], $Item] = aptitems.Items[$Items, $Item]
   
   // ---------------------------------------------------------------------------
   def listMap[K, V](values: Seq[(K, V)])          : ListMap[K, V] = collection.immutable.ListMap.from(values)
--- src/main/scala-3/aptus/AptusScalaVersionSpecific.scala
+++ src/main/scala-3/aptus/AptusScalaVersionSpecific.scala
@@ -5,10 +5,6 @@ trait AptusScalaVersionSpecific { // 3.x
   trait WTT[T] {}; object WTT { implicit def to[T]: WTT[T] = new WTT[T]{} } // TODO
 
   // ---------------------------------------------------------------------------
-  // type Items[$Items <: Items[_, _], $Item] = aptitems.Items[$Items, $Item]
-  //                      ^ Cyclic reference involving type Items (TODO: t241202151014)
-
-  // ---------------------------------------------------------------------------
   def listMap[K, V](values: Seq[(K, V)])          : ListMap[K, V] = collection.immutable.ListMap.from(values)
   def listMap[K, V](value1: (K, V), more: (K, V)*): ListMap[K, V] = listMap[K, V](value1 +: more) }
 

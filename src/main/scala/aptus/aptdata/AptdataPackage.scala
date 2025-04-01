49ab794ff6d119589af4e266b834137398eb2525
Anthony Cros <cros.anthony@gmail.com>
2025-04-01 14:25:35 -0400

    added aptdata package

---
+++ src/main/scala/aptus/aptdata/AptdataPackage.scala
@@ -0,0 +1,27 @@
+package aptus
+
+// ===========================================================================
+package object aptdata
+    extends AptdataAnything
+       with aptus.aptdata.AptusGalliaDataAdaptor
+       with aptus.aptdata.adaptor.SchemaValueExtractions {
+
+  // ---------------------------------------------------------------------------
+  implicit class AptusDataChaining_[A](value: A) {
+    @inline def pype[B](f: A => B): B = f(value) } // TODO: t250122123254 - why
+
+  // ---------------------------------------------------------------------------
+  private[aptdata] type AnyValue = Any }
+
+// ===========================================================================
+package aptdata {
+  trait AptdataAnything {
+      private[aptdata] implicit class AptdataAnything_[A](protected val value: A) extends AptdataAnything__[A] }
+
+    // ---------------------------------------------------------------------------
+    trait AptdataAnything__[A] { protected val value: A
+      private[aptdata] def pipe[B](f: A => B)   : B =   f(value)
+      private[aptdata] def tap    (f: A => Unit): A = { f(value); value } } }
+
+// ===========================================================================
+

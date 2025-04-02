568741611381fe9040f90cb8207d8b615c177a04
Anthony Cros <cros.anthony@gmail.com>
2025-04-02 13:31:27 -0400

    added some accessor tests

--- src/main/scala/aptus/experimental/dyn/tests/DynAccessorsTest.scala
+++ src/main/scala/aptus/experimental/dyn/tests/DynAccessorsTest.scala
@@ -5,14 +5,23 @@ package dyntest
 // ===========================================================================
 object DynAccessorsTest {
   import dyn._
-  import Dyn .dyn
-  import Dyns.dyns
 
   // ===========================================================================
   def main(args: Array[String]): Unit = { apply(); msg(getClass).p }
 
   // ---------------------------------------------------------------------------
   def apply(): Unit = {
-    assert(_Sngl1.string(foo) == bar) } }
+    assert(_Sngl1            .string (foo) ==      bar)
+    assert(_Sngl1            .string_(foo) == Some(bar))
+    assert(_Sngl1.remove(foo).string_(foo) == None)
+
+    assert(_Sngl1z            .strings (foo) ==       List(bar1, bar2))
+    assert(_Sngl1z            .strings_(foo) == Some(List(bar1, bar2)))
+    assert(_Sngl1z.remove(foo).strings_(foo) == None)
+
+    Try { _Sngl1 .strings (foo) }.check(Error.AccessAsSpecificType, BasicType._String)
+    Try { _Sngl1 .strings_(foo) }.check(Error.AccessAsSpecificType, BasicType._String)
+    Try { _Sngl1z.string  (foo) }.check(Error.AccessAsSpecificType, BasicType._String)
+    Try { _Sngl1z.string_ (foo) }.check(Error.AccessAsSpecificType, BasicType._String) } }
 
 // ===========================================================================

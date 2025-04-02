da3618e7ccc3ba1a29f683229d850f9bc0fce868
Anthony Cros <cros.anthony@gmail.com>
2025-04-02 13:31:43 -0400

    added some value extraction related tests

--- src/main/scala/aptus/experimental/dyn/tests/DynMoreTests.scala
+++ src/main/scala/aptus/experimental/dyn/tests/DynMoreTests.scala
@@ -23,6 +23,28 @@ object DynMoreTests {
   // ---------------------------------------------------------------------------
   private def _apply(): Unit = {
 
+    {
+      import aptdata.meta.schema._
+
+      val int  = SubInfo.sngl(_._Int)
+      val str  = SubInfo.sngl(_._String)
+      val nc   = SubInfo.sngl(Cls.cls(Fld.one(qux, _._String)))
+      val info = Info.union(false, int, str, nc)
+
+      val nestingPredicate1: PartialFunction[AnyValue, Boolean] = _.isInstanceOf[Dyn]
+      val nestingPredicate2: PartialFunction[AnyValue, Boolean] = { case _: Dyn => true }
+
+        3  .pipe(info._valueExtractionWithMatching(nestingPredicate1)).check(List(int))
+        3.3.pipe(info._valueExtractionWithMatching(nestingPredicate1)).check(Nil)
+      "abc".pipe(info._valueExtractionWithMatching(nestingPredicate1)).check(List(str))
+
+      Dyn.dyn(foo -> bar).pipe(info._valueExtractionWithMatching(nestingPredicate1)).check(List(nc))
+      Dyn.dyn(foo -> bar).pipe(info._valueExtractionWithMatching(nestingPredicate2)).check(List(nc))
+
+      new java.io.File("myfile").pipe(info._valueExtractionWithMatching(nestingPredicate1)).check(Nil)
+    }
+
+    // ===========================================================================
     // convert
     dyn(foo -> "9", baz -> 1).convertToInt(foo)      .check(dyn(foo -> 9  , baz ->  1))
     dyn(foo -> "9", baz -> 1).convert     (foo).toInt.check(dyn(foo -> 9  , baz ->  1))

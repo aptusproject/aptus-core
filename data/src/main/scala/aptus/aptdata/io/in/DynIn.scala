a3042ed1286c7435b58237e18774649456547ed9
Anthony Cros <cros.anthony@gmail.com>
2025-05-28 10:23:02 -0400

    handling gz for dyn/dyns as well

--- data/src/main/scala/aptus/aptdata/io/in/DynIn.scala
+++ data/src/main/scala/aptus/aptdata/io/in/DynIn.scala
@@ -7,24 +7,30 @@ package in
 private[aptdata] object DynIn { import DynInUtils._
 
   def single(value: InputFilePath): Dyn =
-    FileExtensionSingle
-      .parse(value)
-      .map {
-        case FileExtensionSingle.JsonObjectExtension => jsono(value)
-        case FileExtensionSingle.TsvExtension        => tsvs  (value).forceOne }
-      .orElse { StringContent.parse(value).map {
-        case StringContent.JsonArrayString  =>  value.p__; ??? // TODO: t241029154956
-        case StringContent.JsonObjectString => jsonO(value) } }
-      .getOrElse {  value.p__; ??? }
+    gzipNormalisingOpt(value)
+      .map(FileExtensionSingle.parse)
+      .flatMap(_.map(_single(value)))
+    .orElse {
+      FileExtensionSingle
+        .parse(value)
+        .map(_single(value)) }
+    .orElse { StringContent.parse(value).map {
+      case StringContent.JsonArrayString  =>  value.p__; ??? // TODO: t241029154956
+      case StringContent.JsonObjectString => jsonO(value) } }
+    .getOrElse {  value.p__; ??? }
 
   // ---------------------------------------------------------------------------
   def multiple(value: InputFilePath): Dyns =
+    if (value.endsWith(".gz"))
+      value
+        .stripSuffixGuaranteed(".gz")
+        .pipe(FileExtensionMultiple.parse)
+        .map(_multiple(value))
+        .getOrElse(???)
+    else
     FileExtensionMultiple
       .parse(value)
-      .map {
-        case FileExtensionMultiple.JsonLinesExtension => jsonls(value)
-        case FileExtensionMultiple.JsonArrayExtension => jsonas(value)
-        case FileExtensionMultiple.TsvExtension       => tsvs(value) }
+      .map(_multiple(value))
       .orElse { StringContent.parse(value).map {
         case StringContent.JsonArrayString  => jsonAs(value)
         case StringContent.JsonObjectString =>  value.p__; ??? /* TODO: t241029154956 */ } }
@@ -47,11 +53,30 @@ private[aptdata] object DynIn { import DynInUtils._
             case StringContent.JsonObjectString =>  value.p__; ??? /* TODO: t241029154956 */ } }
           .getOrElse { value.p__; ??? }
 
+  // ===========================================================================
+  private def gzipNormalisingOpt(value: InputFilePath): Option[InputFilePath] =
+    value
+      .in.noneIf(_.endsWith(".gz"))
+      .map(_.stripSuffixGuaranteed(".gz"))
+
+  // ---------------------------------------------------------------------------
+  private def _single(value: String): FileExtensionSingle => Dyn =
+    _ match {
+      case FileExtensionSingle.JsonObjectExtension => jsono(value)
+      case FileExtensionSingle.TsvExtension        => tsvs  (value).forceOne }
+
+  // ---------------------------------------------------------------------------
+  private def _multiple(value: String): FileExtensionMultiple => Dyns =
+    _ match {
+      case FileExtensionMultiple.JsonLinesExtension => jsonls(value) // value.p__; ???
+      case FileExtensionMultiple.JsonArrayExtension => jsonas(value)
+      case FileExtensionMultiple.TsvExtension       => tsvs  (value) }
+
   // ---------------------------------------------------------------------------
   private def _multiplez(value: String): FileExtensionMultiple => Dynz =
     _ match {
       case FileExtensionMultiple.JsonLinesExtension => jsonlz(value) // value.p__; ???
       case FileExtensionMultiple.JsonArrayExtension => jsonaz(value)
-      case FileExtensionMultiple.TsvExtension       => tsvz(value) } }
+      case FileExtensionMultiple.TsvExtension       => tsvz  (value) } }
 
 // ===========================================================================

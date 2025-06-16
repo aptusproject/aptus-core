bb42d6ed62dba5ea07eec9f91d337753d89949d9
Anthony Cros <cros.anthony@gmail.com>
2025-06-16 12:07:42 -0400

    adpated to new Target/TargetData mechanisms

--- data/src/main/scala/aptus/aptdata/AptdataPackage.scala
+++ data/src/main/scala/aptus/aptdata/AptdataPackage.scala
@@ -7,8 +7,10 @@ package object aptdata extends AptdataPackage
 
 // ---------------------------------------------------------------------------
 package aptdata {
+
   trait AptdataPackage
-    extends apttraits.AptusNullShorthands
+    extends AptdataTrait
+       with apttraits.AptusNullShorthands
        with apttraits.AptusDummyImplicitShorthand
 
        // ---------------------------------------------------------------------------
@@ -23,10 +25,7 @@ package aptdata {
        with io.in.TupleBasedBuildingExtensions /* eg ("name": "Alice", "age" -> 30).dyn */
      /* no AptusMinExtensions (since already under aptus) */ {
 
-  private[aptdata] implicit def _symbol2String(value: Symbol): String = value.name
-  private[aptdata] implicit def _string2Symbol(value: String): Symbol = Symbol(value)
-
-  // ---------------------------------------------------------------------------
+  // ===========================================================================
   private[aptdata] type AnyValue = Any
 
   // ===========================================================================
@@ -38,39 +37,55 @@ package aptdata {
 
   // ===========================================================================
   private[aptdata] trait HasTargetSelector {
-      @abstrct protected def target: TargetSelector }
+      @abstrct protected def target: TargetEither }
 
     // ---------------------------------------------------------------------------
-    private[aptdata] trait ValewGetter {
-      @abstrct       protected[aptdata] def get      (key: Key)               : Option[Valew]
-      @nonovrd final protected      def getOrElse(key: Key, alt: => Valew):        Valew = get(key).getOrElse(alt) }
+    private[aptdata] type TargetData = aptus.aptdata.ops.common.td.TargetData
+    private[aptdata] val  TargetData = aptus.aptdata.ops.common.td.TargetData
+
+    // ---------------------------------------------------------------------------
+    private[aptdata] type TargetEitheR = Either[TargetSelector, Targets]
+    private[aptdata] type TargetEither = Either[TargetSelector, TargetData]
+
+    // ---------------------------------------------------------------------------
+    implicit class TargetData_ (protected val self: TargetData) extends ops.common.td.TargetDataOps
 
   // ===========================================================================
-  @publik val _group = "_group"
+  private[aptdata] trait ValewGetter {
+    @abstrct       protected[aptdata] def get      (key: Key)               : Option[Valew]
+    @nonovrd final protected          def getOrElse(key: Key, alt: => Valew):        Valew = get(key).getOrElse(alt) }
+
+  // ===========================================================================
+  private[aptdata] val _group = "_group"
 
   // ===========================================================================
   object selectors {
-    val TargetSelectorShorthands = aptdata.meta.selectors.TargetSelectorShorthands
-    val TargetSelector           = aptdata.meta.selectors.TargetSelector }
+      val TargetSelectorShorthands = aptdata.meta.selectors.TargetSelectorShorthands
+      val TargetSelector           = aptdata.meta.selectors.TargetSelector }
+
+    // ---------------------------------------------------------------------------
+    object shorthands extends shorthands
+      trait shorthands {
+        /** basic types */
+        val $$ = aptus.aptdata.meta.basic.BasicType }
 
   // ===========================================================================
-  object shorthands extends shorthands
-    trait shorthands {
+  @publik implicit class AptDataString_(val u: String) extends io.in.DynIoStringExtensions {
+      /** will work for all but rename & remove (who don't use Target but have explicit Guaranteed counterparts) */
+      def guaranteePresence: Targets = meta.selectors.Target.explicit(u).guaranteePresence }
 
-      // basic types
-      val $$ = aptus.aptdata.meta.basic.BasicType
+    // ---------------------------------------------------------------------------
+    @publik implicit class AptDataSymbol_(val u: Symbol) {
+      /** will work for all but rename & remove (who don't use Target but have explicit Guaranteed counterparts) */
+      def guaranteePresence: Targets = meta.selectors.Target.explicit(u).guaranteePresence }
 
-      // ---------------------------------------------------------------------------
-      // selectors - TODO: worth keeping?
-      object $p { def apply(p: SKeyPred)      : TargetSelector = TargetSelector.Predicate(p) }
-      object $o { def apply(f: SKeys => SKey) : TargetSelector = TargetSelector.SelectOne(f) }
-      object $m { def apply(f: SKeysSelection): TargetSelector = TargetSelector.SelectMultiple(f) } }
+    // ---------------------------------------------------------------------------
+    @publik implicit class AptDataEnumEntry_(val u: enumeratum.EnumEntry) {
+      /** will work for all but rename & remove (who don't use Target but have explicit Guaranteed counterparts) */
+      def guaranteePresence: Targets = meta.selectors.Target.explicit(u).guaranteePresence }
 
   // ===========================================================================
-  @publik implicit class DynIoStringExtensions_(val u: String) extends io.in.DynIoStringExtensions
-
-  // ---------------------------------------------------------------------------
-               implicit class EntriesSeq_ (values:  Seq[(SKey, NakedValue)]) {              def dyn: Dyn = Dyn.build(values.map(Entry._fromString)) }
+                   implicit class EntriesSeq_ (values:  Seq[(SKey, NakedValue)]) {                  def dyn: Dyn = Dyn.build(values.map(Entry._fromString)) }
   private[aptdata] implicit class EntriesSeq2_(values:  Seq[(Key, NakedValue)])  { private[aptdata] def dyn: Dyn = Dyn.build(values.map(Entry._fromKey)) }
   private[aptdata] implicit class EntriesSeq3_(values:  Seq[Entry])              { private[aptdata] def dyn: Dyn = Dyn.build(values) }
 

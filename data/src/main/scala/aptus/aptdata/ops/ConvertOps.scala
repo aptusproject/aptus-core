f1a4128fa5a3cb545ec009480dde93b95ad9465e
Anthony Cros <cros.anthony@gmail.com>
2025-06-16 12:05:26 -0400

    adapted to Target mechanism

--- data/src/main/scala/aptus/aptdata/ops/ConvertOps.scala
+++ data/src/main/scala/aptus/aptdata/ops/ConvertOps.scala
@@ -15,36 +15,34 @@ trait ConvertOps[Data] {
     def toLong   : Data }
 
   // ===========================================================================
-  trait SglConvertOps extends ConvertOps[Sngl] {
-    protected val _ctt: common.CommonTransformTrait[Sngl]
-    protected val _key: Key
-
-    // ---------------------------------------------------------------------------
+  class SglConvertOps private[aptdata] (
+        protected val _ctt    : common.CommonTransformTrait[Sngl],
+        protected val _targets: Targets)
+      extends ConvertOps[Sngl] {
     import _ctt._
 
-    final override def toStr    : Sngl = transform(_key).using { _.format }
-    final override def toBoolean: Sngl = transform(_key).using { _.format /* TODO: t241101134033 or just toString? */ }
+    final override def toStr    : Sngl = transform(_targets).using { _.format }
+    final override def toBoolean: Sngl = transform(_targets).using { _.format /* TODO: t241101134033 or just toString? */ }
 
     /* TODO: more efficient versions of these? */
-    final override def toInt    : Sngl = transform(_key).using { _.format.toInt    }
-    final override def toDouble : Sngl = transform(_key).using { _.format.toDouble }
+    final override def toInt    : Sngl = transform(_targets).using { _.format.toInt    }
+    final override def toDouble : Sngl = transform(_targets).using { _.format.toDouble }
 
-    final override def toLong   : Sngl = transform(_key).using { _.format.toLong } }
+    final override def toLong   : Sngl = transform(_targets).using { _.format.toLong } }
 
   // ===========================================================================
-  trait MultConvertOps[Mult] extends ConvertOps[Mult] {
-    protected val _hem: ops.mult.HasEndoMap[Mult]
-    protected val _key: Key
-
-    // ---------------------------------------------------------------------------
+  class MultConvertOps[Mult] private[aptdata](
+        protected val _hem    : ops.mult.HasEndoMap[Mult],
+        protected val _targets: Targets)
+      extends ConvertOps[Mult] {
     import _hem._
 
-    final override def toBoolean: Mult = endoMap(_.convert(_key).toBoolean)
-    final override def toInt    : Mult = endoMap(_.convert(_key).toInt)
-    final override def toDouble : Mult = endoMap(_.convert(_key).toDouble)
-    final override def toStr    : Mult = endoMap(_.convert(_key).toStr)
+    final override def toBoolean: Mult = endoMap(_.convert(_targets).toBoolean)
+    final override def toInt    : Mult = endoMap(_.convert(_targets).toInt)
+    final override def toDouble : Mult = endoMap(_.convert(_targets).toDouble)
+    final override def toStr    : Mult = endoMap(_.convert(_targets).toStr)
 
   //TODO: codegen
-    final override def toLong  : Mult = endoMap(_.convert(_key).toLong) }
+    final override def toLong  : Mult = endoMap(_.convert(_targets).toLong) }
 
 // ===========================================================================
